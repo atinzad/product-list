70775188735197fd65fcb7f6a3cf7125d844db5f
Ahmad <ahmad_baq@hotmail.com>
2022-02-08 18:26:39 +0300

    done task for middleware

--- apis/products/controllers.js
+++ apis/products/controllers.js
@@ -4,50 +4,56 @@ exports.controllerHelloWorld = (req, res) => {
   res.send("hello world");
 };
 
-exports.controllerGetProducts = async (req, res) => {
+exports.controllerGetProducts = async (req, res, next) => {
   try {
     const products = await Products.find();
     res.json(products);
   } catch (error) {
-    res.status(400);
-    res.json(error.msg);
+    next(error);
   }
 };
 
-exports.controllerAddProduct = async (req, res) => {
+exports.controllerAddProduct = async (req, res, next) => {
   try {
     const product = req.body;
     const productCreated = await Products.create(product);
     res.status(201).json({ msg: "Created", payload: productCreated });
   } catch (error) {
-    res.status(400);
-    res.json({ msg: error.message });
+    next(error);
   }
 };
 
-exports.controllerDeleteProduct = async (req, res) => {
+exports.controllerDeleteProduct = async (req, res, next) => {
   try {
     const { productId } = req.params;
     const productDeleted = await Products.findByIdAndDelete(productId);
     if (productDeleted) {
       res.status(204).end();
     } else {
-      res.status(404).json({ msg: `could not find ${productId}` });
+      const error = new Error(`could not find ${productId}`);
+      error.status = 404;
+      next(error);
     }
   } catch (error) {
-    res.status(400).json({ msg: error.message });
+    next(error);
   }
 };
 
-exports.controllerUpdateProduct = async (req, res) => {
+exports.controllerUpdateProduct = async (req, res, next) => {
   try {
     const { productId } = req.params;
     const product = req.body;
     const productUpdated = await Products.findOneAndUpdate(productId, product, {
       new: true,
     });
-    res.status(200).json({ msg: "Product Updated", payload: productUpdated });
+    if (productUpdated) {
+      res.status(200).json({ msg: "Product Updated", payload: productUpdated });
+    } else {
+      const error = new Error(`could not find ${productId}`);
+      error.status = 404;
+      next(error);
+    }
   } catch (error) {
-    res.status(404).json({ msg: error.message });
+    next(error);
   }
 };
--- app.js
+++ app.js
@@ -7,7 +7,25 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+  );
+  next();
+});
+
 app.use(routes);
+
+app.use((req, res, next) => {
+  res.status(404).json({ msg: "Path Not Found" });
+});
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.status || 500)
+    .json({ msg: error.message || "Internal Server error" });
+  next();
+});
 
 const port = 8000;
 
